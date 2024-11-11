461b8ff0fce863535f977dc882bde754713dd70a
Ricardo gmail <ricardo.andres.fica@gmail.com>
2024-11-11 12:37:12 -0300

    nueva funcion fecha en archivo python

--- python.py
+++ python.py
@@ -4,3 +4,8 @@ def unix_to_timestamp(unix_time):
     dt = datetime.fromtimestamp(unix_time)
     timestamp = dt.strftime('%Y-%m-%d %H:%M:%S')
     return timestamp
+
+def unix_to_date(unix_time):
+    dt = datetime.fromtimestamp(unix_time)
+    date = dt.strftime('%Y-%m-%d %H:%M:%S')
+    return date
